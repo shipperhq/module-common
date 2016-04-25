7a0b8181f6c4c29460b2acc48bfde337d75aa283
Karen Baker <karen@webshopapps.com>
2016-04-25 00:20:52 -0500

    First release

---
+++ src/Helper/AbstractConfig.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ *
+ * ShipperHQ
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * Shipper HQ Shipping
+ *
+ * @category ShipperHQ
+ * @package ShipperHQ_Common
+ * @copyright Copyright (c) 2015 Zowta LLC (http://www.ShipperHQ.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @author ShipperHQ Team sales@shipperhq.com
+ */
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShipperHQ\Common\Helper;
+
+
+use ShipperHQ\Common\Model\ConfigInterface;
+
+/**
+ * Configuration data of carrier
+ */
+abstract class AbstractConfig implements ConfigInterface
+{
+    /**
+     * Get configuration data of carrier
+     *
+     * @param string $type
+     * @param string $code
+     * @return array|string|false
+     */
+    public function getCode($type, $code = '')
+    {
+        $codes = $this->getCodes();
+        if (!isset($codes[$type])) {
+            return false;
+        } elseif ('' === $code) {
+            return $codes[$type];
+        }
+
+        if (!isset($codes[$type][$code])) {
+            return false;
+        } else {
+            return $codes[$type][$code];
+        }
+    }
+
+}

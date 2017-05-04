1363feeb40bf7d08282dcdb37f1ae4c5fe5bf129
wsajason <jason@webshopapps.com>
2017-05-04 11:10:11 +0100

    20.1.5 SHQ16-2004 - custom admin shipping and SHQ16-2029 - code sniffer changes

---
+++ src/Model/Quote/Service.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ *
+ * ShipperHQ
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * Shipper HQ Shipping
+ *
+ * @category ShipperHQ
+ * @package ShipperHQ_Common
+ * @copyright Copyright (c) 2015 Zowta LLC (http://www.ShipperHQ.com)
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @author ShipperHQ Team sales@shipperhq.com
+ */
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace ShipperHQ\Common\Model\Quote;
+
+use ShipperHQ\Lib\Quote\AbstractService;
+
+/**
+ * Class Service
+ *
+ * @package ShipperHQ_Common
+ */
+class Service extends AbstractService
+{
+    /**
+     * Quote repository.
+     *
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $quoteRepository;
+    /**
+     * Shipping method converter
+     *
+     * @var \Magento\Quote\Model\Cart\ShippingMethodConverter
+     */
+    protected $converter;
+    /**
+     * @var \ShipperHQ\Shipper\Helper\LogAssist
+     */
+    private $shipperLogger;
+
+    protected $address;
+    protected $quote;
+
+    /**
+     * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
+     * @param \Magento\Quote\Model\Cart\ShippingMethodConverter $converter
+     * @param \ShipperHQ\Shipper\Helper\LogAssist $shipperLogger
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        \Magento\Quote\Model\Cart\ShippingMethodConverter $converter,
+        \ShipperHQ\Shipper\Helper\LogAssist $shipperLogger
+    ) {
+        $this->quoteRepository = $quoteRepository;
+        $this->converter = $converter;
+        $this->shipperLogger = $shipperLogger;
+    }
+
+    /*
+     * Remove carrier shipping rates
+     */
+    public function cleanDownRates($address, $carrierCode, $carriergroupId, $addressId = false)
+    {
+        $currentRates = $address->getGroupedAllShippingRates();
+
+        foreach ($currentRates as $code => $rates) {
+            //prevent duplicate rates from non-SHQ carriers if enabled
+            if ($code == $carrierCode) {
+                foreach ($rates as $rate) {
+                    if ($carriergroupId == '' || $rate->getCarriergroupId() == $carriergroupId) {
+                        $rate->isDeleted(true);
+                    }
+                }
+            }
+        }
+    }
+}
